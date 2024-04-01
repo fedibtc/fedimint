15939b311c7b3599051105e2af2a527d5660ef9f
Allan Douglas <allandouglas@gmail.com>
2024-04-01 20:04:19 +0000

    feat: FEDIMINT_VETTED_GATEWAYS env var

--- modules/fedimint-ln-server/src/lib.rs
+++ modules/fedimint-ln-server/src/lib.rs
@@ -79,6 +79,29 @@ use crate::db::{
 
 mod metrics;
 
+static FEDIMINT_VETTED_GATEWAYS: fedimint_metrics::Lazy<
+    std::collections::HashSet<secp256k1::PublicKey>,
+> = fedimint_metrics::Lazy::new(|| {
+    let mut ids = std::collections::HashSet::new();
+    for id in std::env::var("FEDIMINT_VETTED_GATEWAYS")
+        .unwrap_or_default()
+        .split(',')
+    {
+        let id = id.trim();
+        if !id.is_empty() {
+            match id.parse() {
+                Ok(id) => {
+                    ids.insert(id);
+                }
+                Err(e) => {
+                    panic!("Gateway id {id} must be valid: {e:?}");
+                }
+            }
+        }
+    }
+    ids
+});
+
 #[derive(Debug, Clone)]
 pub struct LightningInit;
 
@@ -1140,6 +1163,18 @@ impl Lightning {
         dbtx: &mut DatabaseTransaction<'_>,
         gateway: LightningGatewayAnnouncement,
     ) {
+        let gateway_id = gateway.info.gateway_id;
+        if !FEDIMINT_VETTED_GATEWAYS.is_empty() {
+            debug!(
+                "Checking registration against {} vetted gateways",
+                FEDIMINT_VETTED_GATEWAYS.len()
+            );
+            if !FEDIMINT_VETTED_GATEWAYS.contains(&gateway_id) {
+                info!("Ignoring gateway {gateway_id} because it's not on vetted list");
+                return;
+            }
+        }
+
         // Garbage collect expired gateways (since we're already writing to the DB)
         // Note: A "gotcha" of doing this here is that if two gateways are registered
         // at the same time, they will both attempt to delete the same expired gateways
@@ -1147,11 +1182,8 @@ impl Lightning {
         // succeed and the failed one will just try again.
         self.delete_expired_gateways(dbtx).await;
 
-        dbtx.insert_entry(
-            &LightningGatewayKey(gateway.info.gateway_id),
-            &gateway.anchor(),
-        )
-        .await;
+        dbtx.insert_entry(&LightningGatewayKey(gateway_id), &gateway.anchor())
+            .await;
     }
 
     async fn delete_expired_gateways(&self, dbtx: &mut DatabaseTransaction<'_>) {
